3fda542a80cf207b363936edb94cea67cb42e839
okobi45 <okobieigbe@gmail.com>
2023-01-20 23:17:10 +0100

    external Id

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,3 +5,12 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/goverified';
 import millify from 'millify';
+
+
+const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId } }) => (
+    <Link href={`/property/${externalID}`} passHref>
+        {title}
+    </Link>
+)
+
+export default Property
